d0dc36b8f565b75c4067a3b9e640a61afa91adc6
r-ILIASS <iliass.root@gmail.com>
2022-06-04 18:04:45 +0100

    feat[main.js]: add increment decrement functionality and update the cart counter

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let shop = document.getElementById("shop");
 
-let shopItems = [
+const shopItems = [
   {
     id: 1,
     name: "Burger",
@@ -14,56 +14,58 @@ let shopItems = [
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 3,
     name: "Pizza",
     price: 2.5,
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 4,
     name: "Donut",
     price: 0.3,
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 5,
     name: "Taco",
     price: 0.9,
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 6,
     name: "Ice-cream",
     price: 0.5,
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 7,
     name: "Chicken Bucket",
     price: 3,
     img: "./images/hamburger.png",
   },
   {
-    id: 1,
+    id: 8,
     name: "Frensh Fries",
     price: 0.5,
     img: "./images/hamburger.png",
   },
 ];
 
+let basket = [];
+
 let generateShopItems = () => {
   return (shop.innerHTML = shopItems
     .map(
       (item) =>
         `
-      <div class="item">
-        <img src="./images/hamburger.png" alt="item" />
+      <div class="item" id="${item.id}">
+        <img src="${item.img}" alt="${item.name} image" />
         <div class="item__content">
           <h3>${item.name} <span>${item.price}$</span></h3>
           <div class="item__content--controls">
-            <i class="fa-solid fa-minus"></i>
+            <i onclick="decrement(${item.id})" class="fa-solid fa-minus"></i>
             <span class="quantity">0</span>
-            <i class="fa-solid fa-plus"></i>
+            <i onclick="increment(${item.id})" class="fa-solid fa-plus"></i>
           </div>
         </div>
       </div>
@@ -73,3 +75,42 @@ let generateShopItems = () => {
 };
 
 generateShopItems();
+
+const decrement = (id) => {
+  let search = basket.find((basketItem) => basketItem.id === id);
+  if (search === undefined) return;
+  if (search.item === 0) return;
+  search.item--;
+
+  update(id);
+};
+
+const increment = (id) => {
+  let search = basket.find((basketItem) => basketItem.id === id);
+
+  if (search === undefined) {
+    basket.push({ id, item: 1 });
+  } else {
+    search.item++;
+  }
+
+  update(id);
+};
+
+const update = (id) => {
+  let search = basket.find((basketItem) => basketItem.id === id);
+
+  document.getElementById(id).querySelector(".quantity").innerHTML =
+    search.item;
+
+  itemsSum();
+};
+
+const itemsSum = () => {
+  let cartCounter = document.querySelector(".cartCounter");
+
+  let count = 0;
+  basket.forEach(({ item }) => (count += item));
+
+  cartCounter.innerHTML = count;
+};
