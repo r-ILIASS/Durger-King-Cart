df6bca41422d27e51de4250cb7837a914cb9afae
r-ILIASS <iliass.root@gmail.com>
2022-06-06 14:08:53 +0100

    refactor: extract dummy data to a separate file

--- src/cart.js
+++ src/cart.js
@@ -2,10 +2,9 @@ let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const itemsSum = () => {
   let cartCounter = document.querySelector(".cartCounter");
-
   let count = 0;
-  basket.forEach(({ item }) => (count += item));
 
+  basket.forEach(({ item }) => (count += item));
   cartCounter.innerHTML = count;
 };
 
---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+const shopItems = [
+  {
+    id: 1,
+    name: "XL Burger",
+    price: 1.3,
+    img: "./images/hamburger.png",
+  },
+  {
+    id: 2,
+    name: "Hotdog + Drink",
+    price: 0.5,
+    img: "./images/hotdog.png",
+  },
+  {
+    id: 3,
+    name: "Pizza",
+    price: 2.5,
+    img: "./images/pizza.png",
+  },
+  {
+    id: 4,
+    name: "Donut",
+    price: 0.3,
+    img: "./images/donut.png",
+  },
+  {
+    id: 5,
+    name: "Taco",
+    price: 0.9,
+    img: "./images/taco.png",
+  },
+  {
+    id: 6,
+    name: "Ice-cream",
+    price: 0.5,
+    img: "./images/ice-cream.png",
+  },
+  {
+    id: 7,
+    name: "Chicken Bucket",
+    price: 3,
+    img: "./images/chicken-bucket.png",
+  },
+  {
+    id: 8,
+    name: "French Fries",
+    price: 0.5,
+    img: "./images/french-fries.png",
+  },
+];
